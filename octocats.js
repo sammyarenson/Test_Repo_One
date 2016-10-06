022137ed12004133d4604058b56e86adbd47405b
sammyarenson <9sarenson@stpatrick.org>
2016-10-06 14:10:48 -0500

    Octocats day 1
    started writing for loop in octo cat

---
+++ octocats.js
@@ -0,0 +1,22 @@
+//Write your constructor function below.
+//You should have 2 instance variables and 1 instance function included.
+function Octocat(n,a){
+  this.names= n;
+  this.arms= a;
+
+  this.slap= function(){
+    for(var a=0,a++)
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+//Write your 3 new octocat objects below here.
